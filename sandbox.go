8af110c11e28c01578318a924c2b41c8806548f0
James Hunt <james@niftylogic.com>
2016-01-30 20:21:17 -0500

    Run(Options{}) approach

--- sandbox.go
+++ sandbox.go
@@ -1,20 +1,67 @@
+/*
+Package sandbox provides a safe and hassle-free way of running
+arbitrarily complex commands, with potentially harmful
+side-effects, without incurring the risk to the host system.
+*/
 package sandbox
 
 import (
+	"fmt"
 	"bytes"
 	docker "github.com/fsouza/go-dockerclient"
 )
 
-func Run(command string) (int, string, error) {
-	client, err := docker.NewClient("unix:///var/run/docker.sock")
+// Options is used to specify configuration values for the Run
+// function.
+type Options struct {
+	// Endpoint specifies the URL at which to contact Docker daemon
+	// for running containers.
+	// Defaults to `unix:///var/run/docker.sock`
+	Endpoint string
+
+	// Image identifies the name (or URL) of the Docker image to
+	// instantiate as a sandbox container.
+	// Defaults to `ubuntu`
+	Image    string
+
+	// Command is the set of commands that will be fed to
+	// `bash -lc`.  Multiple commands can be separated by newlines,
+	// or other Bash command separators, like ';' and '&&'.
+	// It is an error to omit this option.
+	Command  string
+
+	// How long to allow the command to execute before forcibly
+	// timing it out (in seconds).
+	// Defaults to 30.
+	Timeout  int
+}
+
+// Run spins up a new sandbox container, issues the command(s), and
+// then waits for the exit code and output (both standard output
+// and standard error, intermixed).
+func Run(opt Options) (int, string, error) {
+	if opt.Endpoint == "" {
+		opt.Endpoint = "unix:///var/run/docker.sock"
+	}
+	if opt.Image == "" {
+		opt.Image = "ubuntu"
+	}
+	if opt.Timeout <= 0 {
+		opt.Timeout = 30
+	}
+	if opt.Command == "" {
+		return -1, "", fmt.Errorf("no command specified")
+	}
+
+	client, err := docker.NewClient(opt.Endpoint)
 	if err != nil {
 		return -1, "", err
 	}
 
 	c, err := client.CreateContainer(docker.CreateContainerOptions{
 		Config: &docker.Config{
-			Image: "ubuntu",
-			Cmd:   []string{"/bin/bash", "-l", "-c", command},
+			Image: opt.Image,
+			Cmd:   []string{"timeout", "--preserve-status", fmt.Sprintf("%ds", opt.Timeout), "/bin/bash", "-l", "-c", opt.Command},
 			Tty:   true,
 		},
 	})
